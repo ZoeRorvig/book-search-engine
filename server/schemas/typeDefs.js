f00b961e082c55904d9fbbd5e9eab552393a79d5
Zoe Rorvig <zrorvig@gmail.com>
2023-02-11 16:45:13 -0500

    fixed addUser and login

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,21 +2,21 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID
+    _id: ID!
     username: String
     email: String
-    password: String
+    bookCount: Int
     savedBooks: [Book]
   }
 
   type Book {
-    _id: ID
+    _id: ID!
     bookId: String
     authors: [String]
     description: String
+    title: String
     image: String
     link: String
-    title: String
   }
 
   input BookInput {
@@ -30,7 +30,7 @@ const typeDefs = gql`
 
   type Auth {
     token: ID!
-    profile: User
+    user: User
   }
 
   type Query {
