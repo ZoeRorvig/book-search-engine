d7e8664bb3d1c4e41115f24c736c34f8d3ad68d3
Zoe Rorvig <zrorvig@gmail.com>
2023-02-11 17:17:49 -0500

    updated server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,7 +6,7 @@ const resolvers = {
     Query: {
         me: async (_parent, args, context) => {
             if (context.user) {
-                return User.findOne({  _id: context.user._id });
+                return User.findOne({ _id: context.user._id });
             }
             throw new AuthenticationError('You need to be logged in!');
         },
@@ -33,28 +33,34 @@ const resolvers = {
         addUser: async (_parent, args) => {
             const user = await User.create(args);
             const token = signToken(user);
-            
+
             return { token, user };
         },
 
-        saveBook: async (_parent, { user, body }) => {
-            const updatedUser = await User.findOneAndUpdate(
-                { _id: user._id },
-                { $addToSet: { savedBooks: body } },
-                { new: true, runValidators: true }
-            );
+        saveBook: async (_parent, { input }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: input } },
+                    { new: true, runValidators: true }
+                );
+                return updatedUser;
+            }
             throw new AuthenticationError('You need to be logged in!');
         },
 
-        removeBook: async (_parent, {user, book}) => {
-            const updatedUser = await User.findOneAndUpdate(
-                { _id: user._id },
-                { $pull: { savedBooks: { bookId: book.bookId } } },
-                { new: true }
-            );
-            throw new AuthenticationError('You need to be logged in!');
+        removeBook: async (_parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+                throw new AuthenticationError('You need to be logged in!');
+            },
         },
-    },
-};
+    };
 
-module.exports = resolvers;
+    module.exports = resolvers;
